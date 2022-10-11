91ea13ff3b199bbbcc3ddd2472a6b91d2b76b453
Roman Barlos <rbarlos@yandex-team.ru>
2022-10-11 13:10:44 +0300

    fix: import svg (#14)

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import svgr from '@svgr/rollup';
 import scss from 'rollup-plugin-scss';
 import json from 'rollup-plugin-json';
 
@@ -30,6 +31,7 @@ export default [
                 tsconfig: './tsconfig.publish.json',
             }),
             scss(),
+            svgr(),
         ],
     },
 ];
