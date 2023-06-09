a658e191cc2732c6c76f0380e34cab8c181f4c30
Angelina Gadzhieva <lunory@yandex-team.ru>
2023-06-09 16:51:31 +0300

    chore: reexport Title

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export {ActionBar} from './ActionBar';
 export {AsideHeader, AsideHeaderProps} from './AsideHeader/AsideHeader';
 export {Drawer, DrawerProps, DrawerItemProps, DrawerItem} from './Drawer/Drawer';
 export {FooterItem, FooterItemProps} from './FooterItem/FooterItem';
+export * from './Title';
 export * from './HotkeysPanel';
 export * from './Settings';
 export * from './types';
