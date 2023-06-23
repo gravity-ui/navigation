9baaa334e4aa2dda77831b9e2e0c98c6d704fada
Sergey S. Volkov <js2me@outlook.com>
2023-06-23 16:08:31 +0300

    feat: make CompositeBar, CompositeBarProps exportable

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export {ActionBar} from './ActionBar';
 export {AsideHeader, AsideHeaderProps} from './AsideHeader/AsideHeader';
+export {CompositeBar, CompositeBarProps} from './CompositeBar/CompositeBar';
 export {Drawer, DrawerProps, DrawerItemProps, DrawerItem} from './Drawer/Drawer';
 export {FooterItem, FooterItemProps} from './FooterItem/FooterItem';
 export * from './Title';
