3e3cfef50129527835eb73fa299a412dd6e3b3e7
sunduckcow <sunduckcow@yandex-team.ru>
2023-07-04 12:36:04 +0300

    feat: added type exports for ActionBar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-export {ActionBar} from './ActionBar';
 export {AsideHeader, AsideHeaderProps} from './AsideHeader/AsideHeader';
 export {Drawer, DrawerProps, DrawerItemProps, DrawerItem} from './Drawer/Drawer';
 export {FooterItem, FooterItemProps} from './FooterItem/FooterItem';
+export * from './ActionBar';
 export * from './Title';
 export * from './HotkeysPanel';
 export * from './Settings';
