96dbcf39863aea6ea781a224636b4abc6f5a64e5
sunduckcow <83990793+sunduckcow@users.noreply.github.com>
2023-07-04 12:33:38 +0300

    feat: added new stories for ActionBar (#64)
    Co-authored-by: sunduckcow <sunduckcow@yandex-team.ru>

---
+++ src/components/ActionBar/__stories__/ActionBarSingleSection.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {ActionBar} from '../ActionBar';
+import {Breadcrumbs, Button, ClipboardButton, DropdownMenu} from '@gravity-ui/uikit';
+
+export function ActionBarSingleSection() {
+    return (
+        <ActionBar>
+            <ActionBar.Section type="primary">
+                <ActionBar.Group>
+                    <ActionBar.Item>
+                        <Breadcrumbs
+                            lastDisplayedItemsCount={1}
+                            items={[
+                                {text: 'Devtools Support', action() {}},
+                                {text: 'DEVTOOLSSUPPORT-21001', action() {}},
+                            ]}
+                            firstDisplayedItemsCount={1}
+                        />
+                    </ActionBar.Item>
+
+                    <ActionBar.Item spacing={false}>
+                        <ClipboardButton size={16} text={'DEVTOOLSSUPPORT-21001'} />
+                    </ActionBar.Item>
+                </ActionBar.Group>
+
+                <ActionBar.Group pull="right">
+                    <ActionBar.Item>
+                        <Button view="flat">Some users</Button>
+                    </ActionBar.Item>
+
+                    <ActionBar.Separator />
+
+                    <ActionBar.Item>
+                        <Button view="flat">Macros</Button>
+                    </ActionBar.Item>
+
+                    <ActionBar.Item>
+                        <DropdownMenu
+                            switcher={<Button>Actions</Button>}
+                            items={[{text: 'Convert to sub-issue', action() {}}]}
+                        />
+                    </ActionBar.Item>
+                </ActionBar.Group>
+            </ActionBar.Section>
+        </ActionBar>
+    );
+}
