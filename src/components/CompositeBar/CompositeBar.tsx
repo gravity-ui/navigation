405ce8730ee49f977c1cdeeb454853f8d16184b8
sunduckcow <sunduckcow@yandex-team.ru>
2023-07-06 11:24:23 +0300

    feat(CompositeBar): rewritten to FC

--- src/components/CompositeBar/CompositeBar.tsx
+++ src/components/CompositeBar/CompositeBar.tsx
@@ -1,193 +1,118 @@
-import React from 'react';
+import React, {FC, useRef} from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import {List} from '@gravity-ui/uikit';
 
 import {block} from '../utils/cn';
-import {AsideHeaderDict, Dict, MenuItem} from './../types';
-import {getItemsHeight, getItemHeight, getSelectedItemIndex, getItemsMinHeight} from './utils';
-import {COLLAPSE_ITEM_ID} from './constants';
-import {defaultDict} from './../constants';
+import {AsideHeaderDict, MenuItem} from './../types';
+import {
+    getItemsHeight,
+    getItemHeight,
+    getSelectedItemIndex,
+    getItemsMinHeight,
+    getMoreButtonItem,
+    getAutosizeListItems,
+} from './utils';
 import {Item} from './Item/Item';
-
-import dotsIcon from '../../../assets/icons/dots.svg';
 
 import './CompositeBar.scss';
 
 const b = block('composite-bar');
 
-export interface CompositeBarProps {
+interface CompositeBarBaseProps {
     items: MenuItem[];
     compact: boolean;
-    enableCollapsing: boolean;
-    dict?: AsideHeaderDict;
     onItemClick?: (item: MenuItem, collapsed: boolean) => void;
 }
 
-interface CompositeBarState {
-    activeItemIndex?: number;
+interface CompositeBarViewProps extends CompositeBarBaseProps {
+    collapseItems?: MenuItem[];
 }
 
-export class CompositeBar extends React.Component<CompositeBarProps> {
-    state: CompositeBarState = {
-        activeItemIndex: undefined,
-    };
-    private listRef = React.createRef<List<MenuItem>>();
+export interface CompositeBarProps extends CompositeBarBaseProps {
+    enableCollapsing: boolean;
+    dict?: AsideHeaderDict;
+}
 
-    render() {
-        const {enableCollapsing, items} = this.props;
+const CompositeBarView: FC<CompositeBarViewProps> = ({
+    items,
+    compact,
+    onItemClick,
+    collapseItems,
+}) => {
+    const ref = useRef<List<MenuItem>>(null);
+    return (
+        <List<MenuItem>
+            ref={ref}
+            items={items}
+            selectedItemIndex={getSelectedItemIndex(items)}
+            itemHeight={getItemHeight}
+            itemClassName={b('root-menu-item')}
+            itemsHeight={getItemsHeight}
+            virtualized={false}
+            filterable={false}
+            sortable={false}
+            renderItem={(item) => (
+                <Item
+                    item={item}
+                    onMouseLeave={() => {
+                        if (compact) {
+                            ref.current?.activateItem(undefined as unknown as number);
+                        }
+                    }}
+                    compact={compact}
+                    collapseItems={collapseItems}
+                    onItemClick={onItemClick}
+                />
+            )}
+        />
+    );
+};
 
-        if (items.length === 0) {
-            return null;
-        }
+export const CompositeBar: FC<CompositeBarProps> = ({
+    items,
+    compact,
+    enableCollapsing,
+    dict,
+    onItemClick,
+}) => {
+    if (items.length === 0) {
+        return null;
+    }
 
-        if (!enableCollapsing) {
-            return <div className={b()}>{this.renderMenu()}</div>;
-        }
-
-        const minHeight = getItemsMinHeight(items);
-
+    if (!enableCollapsing) {
         return (
-            <div className={b({autosizer: true})} style={{minHeight}}>
-                {items.length !== 0 && (
-                    <AutoSizer>
-                        {({width, height}) => {
-                            const style = {
-                                width,
-                                height,
-                            };
-                            return <div style={style}>{this.renderAutosizeMenu(height)}</div>;
-                        }}
-                    </AutoSizer>
-                )}
+            <div className={b()}>
+                <CompositeBarView items={items} compact={compact} onItemClick={onItemClick} />
             </div>
         );
     }
 
-    private renderAutosizeMenu(height: number) {
-        const {compact, onItemClick} = this.props;
+    const minHeight = getItemsMinHeight(items);
+    const collapseItem = getMoreButtonItem(dict);
 
-        const {listItems, collapseItems} = this.getAutosizeListItems(height);
-
-        return (
-            <List<MenuItem>
-                ref={this.listRef}
-                items={listItems}
-                selectedItemIndex={getSelectedItemIndex(listItems)}
-                itemHeight={getItemHeight}
-                itemClassName={b('root-menu-item')}
-                itemsHeight={getItemsHeight}
-                virtualized={false}
-                filterable={false}
-                sortable={false}
-                renderItem={(item) => (
-                    <Item
-                        item={item}
-                        onMouseLeave={() => {
-                            if (compact) {
-                                this.listRef.current?.activateItem(undefined as unknown as number);
-                            }
-                        }}
-                        compact={compact}
-                        collapseItems={collapseItems}
-                        onItemClick={onItemClick}
-                    />
-                )}
-            />
-        );
-    }
-
-    private renderMenu() {
-        const {items, onItemClick, compact} = this.props;
-
-        return (
-            <List<MenuItem>
-                ref={this.listRef}
-                items={items}
-                selectedItemIndex={getSelectedItemIndex(items)}
-                itemHeight={getItemHeight}
-                itemClassName={b('root-menu-item')}
-                itemsHeight={getItemsHeight}
-                virtualized={false}
-                filterable={false}
-                sortable={false}
-                renderItem={(item) => (
-                    <Item
-                        item={item}
-                        onMouseLeave={() => {
-                            if (compact) {
-                                this.listRef.current?.activateItem(undefined as unknown as number);
-                            }
-                        }}
-                        compact={compact}
-                        onItemClick={onItemClick}
-                    />
-                )}
-            />
-        );
-    }
-
-    private getMoreButtonItem(): MenuItem {
-        const {dict} = this.props;
-        const title = dict?.[Dict.MoreButton] ?? defaultDict[Dict.MoreButton];
-
-        return {
-            id: COLLAPSE_ITEM_ID,
-            title,
-            icon: dotsIcon,
-            iconSize: 16,
-        };
-    }
-
-    private getAutosizeListItems(height: number): {
-        listItems: MenuItem[];
-        collapseItems: MenuItem[];
-    } {
-        const {items} = this.props;
-
-        const afterMoreButtonItems = items.filter((item) => item.afterMoreButton);
-        const regularItems = items.filter((item) => !item.afterMoreButton);
-        const listItems = [...regularItems, ...afterMoreButtonItems];
-
-        const allItemsHeight = getItemsHeight(listItems);
-        if (allItemsHeight <= height) {
-            return {listItems, collapseItems: []};
-        }
-
-        const collapseItem = this.getMoreButtonItem();
-        const collapseItemHeight = getItemHeight(collapseItem);
-
-        listItems.splice(regularItems.length, 0, collapseItem);
-        const collapseItems: MenuItem[] = [];
-
-        let listHeight = allItemsHeight + collapseItemHeight;
-        let index = listItems.length;
-        while (listHeight > height) {
-            if (index === 0) {
-                break;
-            }
-            index--;
-
-            const item = listItems[index];
-            if (item.pinned || item.id === COLLAPSE_ITEM_ID || item.afterMoreButton) {
-                continue;
-            }
-            if (item.type === 'divider') {
-                if (index + 1 < listItems.length && listItems[index + 1]?.type === 'divider') {
-                    listHeight -= getItemHeight(item);
-                    listItems.splice(index, 1);
-                }
-                continue;
-            }
-            listHeight -= getItemHeight(item);
-            collapseItems.unshift(...listItems.splice(index, 1));
-        }
-        if (
-            listItems[index]?.type === 'divider' &&
-            (index === 0 || listItems[index - 1]?.type === 'divider')
-        ) {
-            listItems.splice(index, 1);
-        }
-
-        return {listItems, collapseItems};
-    }
-}
+    return (
+        <div className={b({autosizer: true})} style={{minHeight}}>
+            {items.length !== 0 && (
+                <AutoSizer>
+                    {({width, height}) => {
+                        const {listItems, collapseItems} = getAutosizeListItems(
+                            items,
+                            height,
+                            collapseItem,
+                        );
+                        return (
+                            <div style={{width, height}}>
+                                <CompositeBarView
+                                    items={listItems}
+                                    compact={compact}
+                                    onItemClick={onItemClick}
+                                    collapseItems={collapseItems}
+                                />
+                            </div>
+                        );
+                    }}
+                </AutoSizer>
+            )}
+        </div>
+    );
+};
