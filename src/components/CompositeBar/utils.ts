78f43b04c8eaa155a44e6953130476a85d0a4b40
sunduckcow <83990793+sunduckcow@users.noreply.github.com>
2023-07-06 15:50:49 +0300

    feat(CompositeBar): rewritten to FC (#67)
    Co-authored-by: sunduckcow <sunduckcow@yandex-team.ru>

--- src/components/CompositeBar/utils.ts
+++ src/components/CompositeBar/utils.ts
@@ -1,5 +1,7 @@
-import {MenuItem} from './../types';
-import {ITEM_HEIGHT} from './constants';
+import {Ellipsis} from '@gravity-ui/icons';
+import {AsideHeaderDict, Dict, MenuItem} from './../types';
+import {COLLAPSE_ITEM_ID, ITEM_HEIGHT} from './constants';
+import {defaultDict} from '../constants';
 
 export function getItemHeight(item: MenuItem) {
     switch (item.type) {
@@ -45,4 +47,69 @@ export function getItemsMinHeight(items: MenuItem[]) {
         getItemsHeight(afterMoreButtonItems) +
         (pinnedItems.length === items.length ? 0 : ITEM_HEIGHT)
     );
+}
+
+export function getMoreButtonItem(dict?: AsideHeaderDict): MenuItem {
+    const title = dict?.[Dict.MoreButton] ?? defaultDict[Dict.MoreButton];
+
+    return {
+        id: COLLAPSE_ITEM_ID,
+        title,
+        icon: Ellipsis,
+        iconSize: 18,
+    };
+}
+
+export function getAutosizeListItems(
+    items: MenuItem[],
+    height: number,
+    collapseItem: MenuItem,
+): {
+    listItems: MenuItem[];
+    collapseItems: MenuItem[];
+} {
+    const afterMoreButtonItems = items.filter((item) => item.afterMoreButton);
+    const regularItems = items.filter((item) => !item.afterMoreButton);
+    const listItems = [...regularItems, ...afterMoreButtonItems];
+
+    const allItemsHeight = getItemsHeight(listItems);
+    if (allItemsHeight <= height) {
+        return {listItems, collapseItems: []};
+    }
+
+    const collapseItemHeight = getItemHeight(collapseItem);
+
+    listItems.splice(regularItems.length, 0, collapseItem);
+    const collapseItems: MenuItem[] = [];
+
+    let listHeight = allItemsHeight + collapseItemHeight;
+    let index = listItems.length;
+    while (listHeight > height) {
+        if (index === 0) {
+            break;
+        }
+        index--;
+
+        const item = listItems[index];
+        if (item.pinned || item.id === COLLAPSE_ITEM_ID || item.afterMoreButton) {
+            continue;
+        }
+        if (item.type === 'divider') {
+            if (index + 1 < listItems.length && listItems[index + 1]?.type === 'divider') {
+                listHeight -= getItemHeight(item);
+                listItems.splice(index, 1);
+            }
+            continue;
+        }
+        listHeight -= getItemHeight(item);
+        collapseItems.unshift(...listItems.splice(index, 1));
+    }
+    if (
+        listItems[index]?.type === 'divider' &&
+        (index === 0 || listItems[index - 1]?.type === 'divider')
+    ) {
+        listItems.splice(index, 1);
+    }
+
+    return {listItems, collapseItems};
 }
