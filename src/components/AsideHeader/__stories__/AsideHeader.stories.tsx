74b12fa4c1531ac2c981eee03f64242c55242473
sunduckcow <sunduckcow@yandex-team.ru>
2023-07-17 16:47:18 +0300

    fix: returned compact story back

--- src/components/AsideHeader/__stories__/AsideHeader.stories.tsx
+++ src/components/AsideHeader/__stories__/AsideHeader.stories.tsx
@@ -12,6 +12,12 @@ export default {
 const ShowcaseTemplate: Story = (args) => <AsideHeaderShowcase {...args} />;
 export const Showcase = ShowcaseTemplate.bind({});
 
+const CompactTemplate: Story = (args) => <AsideHeaderShowcase {...args} />;
+export const Compact = CompactTemplate.bind({});
+Compact.args = {
+    initialCompact: true,
+};
+
 const MultipleTooltipTemplate: Story = (args) => <AsideHeaderShowcase {...args} />;
 export const MultipleTooltip = MultipleTooltipTemplate.bind({});
 MultipleTooltip.args = {
