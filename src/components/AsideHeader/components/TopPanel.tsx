6e9e767f12ef42cf13f19e0f026777e831ff0657
Georgy Malkov <malkovgv@yandex-team.ru>
2023-10-27 18:30:41 +0300

    fix: divider color with alpha chanel

--- src/components/AsideHeader/components/TopPanel.tsx
+++ src/components/AsideHeader/components/TopPanel.tsx
@@ -4,7 +4,11 @@ import {Alert} from '@gravity-ui/uikit';
 import {useAsideHeaderInnerContext} from '../AsideHeaderContext';
 import {b} from '../utils';
 
-export const TopPanel = React.forwardRef<HTMLDivElement>((_, ref) => {
+type Props = {
+    onClose: () => void;
+};
+
+export const TopPanel = React.forwardRef<HTMLDivElement, Props>(({onClose}, ref) => {
     const {topAlert} = useAsideHeaderInnerContext();
 
     const [opened, setOpened] = React.useState(true);
@@ -13,6 +17,12 @@ export const TopPanel = React.forwardRef<HTMLDivElement>((_, ref) => {
         setOpened(false);
     }, []);
 
+    React.useEffect(() => {
+        if (!opened) {
+            onClose();
+        }
+    }, [opened, onClose]);
+
     if (!topAlert) {
         return null;
     }
@@ -20,17 +30,20 @@ export const TopPanel = React.forwardRef<HTMLDivElement>((_, ref) => {
     return (
         <div ref={ref} className={b('pane-top', {opened})}>
             {opened && (
-                <Alert
-                    className={b('pane-top-alert', {centered: topAlert.centered})}
-                    corners="square"
-                    layout="horizontal"
-                    theme={topAlert.theme || 'warning'}
-                    icon={topAlert.icon}
-                    title={topAlert.title}
-                    message={topAlert.message}
-                    actions={topAlert.actions}
-                    onClose={topAlert.closable ? handleClose : undefined}
-                />
+                <React.Fragment>
+                    <Alert
+                        className={b('pane-top-alert', {centered: topAlert.centered})}
+                        corners="square"
+                        layout="horizontal"
+                        theme={topAlert.theme || 'warning'}
+                        icon={topAlert.icon}
+                        title={topAlert.title}
+                        message={topAlert.message}
+                        actions={topAlert.actions}
+                        onClose={topAlert.closable ? handleClose : undefined}
+                    />
+                    <div className={b('pane-top-divider')}></div>
+                </React.Fragment>
             )}
         </div>
     );
