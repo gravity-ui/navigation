0fe50f59e1b82a3e9875e3d2ddcf26062b9157f1
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-06-28 13:52:10 +0200

    fix: update test

--- src/components/AsideHeader/__tests__/AsideHeader.visual.test.tsx
+++ src/components/AsideHeader/__tests__/AsideHeader.visual.test.tsx
@@ -6,55 +6,91 @@ import {AsideHeaderStories} from './helpersPlaywright';
 
 test.describe('AsideHeader', () => {
     test('render story: <AdvancedUsage>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.AdvancedUsage />);
+        await mount(<AsideHeaderStories.AdvancedUsage />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <Compact>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.Compact />);
+        await mount(<AsideHeaderStories.Compact />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <CustomBackground>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.CustomBackground />);
+        await mount(<AsideHeaderStories.CustomBackground />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <CustomTheme>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.CustomTheme />);
+        await mount(<AsideHeaderStories.CustomTheme />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <Fallback>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.Fallback />);
+        await mount(<AsideHeaderStories.Fallback />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <HeaderAlert>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.HeaderAlert />);
+        await mount(<AsideHeaderStories.HeaderAlert />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <HeaderAlertCentered>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.HeaderAlertCentered />);
+        await mount(<AsideHeaderStories.HeaderAlertCentered />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <MultipleTooltip>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.MultipleTooltip />);
+        await mount(<AsideHeaderStories.MultipleTooltip />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
 
     test('render story: <Showcase>', async ({mount, expectScreenshot}) => {
-        await mount(<AsideHeaderStories.Showcase />);
+        await mount(<AsideHeaderStories.Showcase />, undefined, {
+            padding: 20,
+            width: 390,
+            height: 844,
+        });
 
         await expectScreenshot();
     });
