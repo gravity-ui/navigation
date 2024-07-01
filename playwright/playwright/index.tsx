cc1da6d8a686fae6684ec18e24a24fccb96d8527
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-07-01 15:25:41 +0200

    fix: fix type

--- playwright/playwright/index.tsx
+++ playwright/playwright/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import {MobileProvider, ThemeProvider} from '@gravity-ui/uikit';
 import {beforeMount} from '@playwright/experimental-ct-react/hooks';
 
-/* import {PageLayout} from '../../src/components/AsideHeader/components/PageLayout/PageLayout'; */
-
 import './index.scss';
 
 beforeMount(async ({App}) => {
