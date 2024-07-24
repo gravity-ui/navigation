443233110eec2942bc260de2e254d40a8b08f4aa
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-07-24 11:31:43 +0200

    fix: workers playwright config

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -41,7 +41,7 @@ const config: PlaywrightTestConfig = {
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 8 : undefined,
+    workers: 2,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter,
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -67,6 +67,8 @@ const config: PlaywrightTestConfig = {
                 alias: {
                     '~playwright': resolve(__dirname),
                     '~@gravity-ui/uikit/styles/mixins': '@gravity-ui/uikit/styles/mixins',
+                    '~@doc-tools/transform/dist/css/yfm.css':
+                        '@doc-tools/transform/dist/css/yfm.css',
                 },
             },
         },
