6123d2c262d2cc0ea584811daa56c8d5eece514e
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-08-23 18:23:44 +0200

    test: playwright update change theme and add providers

--- playwright/playwright/index.tsx
+++ playwright/playwright/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import {MobileProvider, ThemeProvider} from '@gravity-ui/uikit';
+import {MobileProvider, ThemeProvider, ToasterComponent, ToasterProvider} from '@gravity-ui/uikit';
 import {beforeMount} from '@playwright/experimental-ct-react/hooks';
 
 import './index.scss';
 
 beforeMount(async ({App}) => {
     return (
-        <ThemeProvider theme="light">
+        <ThemeProvider>
             <MobileProvider>
-                <App />
+                <ToasterProvider>
+                    <App />
+                    <ToasterComponent />
+                </ToasterProvider>
             </MobileProvider>
         </ThemeProvider>
     );
