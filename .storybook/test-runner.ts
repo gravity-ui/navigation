362b0664a152483b2993ad1b675b1f1bda44a46b
Roman Barlos <rbarlos@yandex-team.ru>
2025-01-23 18:59:57 +0300

    feat: rollup v4 (#319)
    * feat: rollup v4
    
    feat: migrate to rollup v4
    
    * fix: run storybook tests

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,5 +1,5 @@
 import type {TestRunnerConfig} from '@storybook/test-runner';
-import {getStoryContext} from '@storybook/test-runner';
+import {getStoryContext, waitForPageReady} from '@storybook/test-runner';
 import {checkA11y, configureAxe, injectAxe} from 'axe-playwright';
 
 /*
@@ -8,11 +8,13 @@ import {checkA11y, configureAxe, injectAxe} from 'axe-playwright';
  */
 const config: TestRunnerConfig = {
     async preVisit(page) {
+        await waitForPageReady(page);
         await injectAxe(page);
     },
     async postVisit(page, context) {
         // Get the entire context of a story, including parameters, args, argTypes, etc.
         const storyContext = await getStoryContext(page, context);
+        await waitForPageReady(page);
 
         // Apply story-level a11y rules
         await configureAxe(page, {
@@ -20,6 +22,8 @@ const config: TestRunnerConfig = {
             reporter: 'no-passes',
         });
 
+        // hack for prevent error "Axe is already running"
+        await new Promise(resolve => setTimeout(resolve, 100));
         await checkA11y(page, '#storybook-root', {
             verbose: false,
             detailedReport: true,
