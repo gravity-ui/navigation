f9c96b297bd7234c87cf4efe4b3cb818792d3229
Angelina Gadzhieva <lunory@yandex-team.ru>
2025-02-10 14:43:56 +0300

    fix(MultipleTooltip): fix popup styles and offset

--- src/components/CompositeBar/MultipleTooltip/MultipleTooltip.tsx
+++ src/components/CompositeBar/MultipleTooltip/MultipleTooltip.tsx
@@ -12,7 +12,7 @@ import './MultipleTooltip.scss';
 
 const b = block('multiple-tooltip');
 
-const POPUP_OFFSET: PopupProps['offset'] = {mainAxis: -32, crossAxis: 4};
+const POPUP_OFFSET: PopupProps['offset'] = {mainAxis: 4, crossAxis: -32};
 
 export type MultipleTooltipProps = Pick<PopupProps, 'open' | 'anchorRef' | 'placement'> & {
     items: MenuItem[];
@@ -28,35 +28,43 @@ export const MultipleTooltip: React.FC<MultipleTooltipProps> = ({
     const activeItem = activeIndex === undefined ? null : items[activeIndex];
 
     return (
-        <Popup open={open} anchorRef={anchorRef} placement={placement} offset={POPUP_OFFSET}>
+        <Popup
+            open={open}
+            className={b('popup')}
+            anchorRef={anchorRef}
+            placement={placement}
+            offset={POPUP_OFFSET}
+        >
             <div className={b()}>
-                {items
-                    .filter(
-                        ({type = 'regular', id}) =>
-                            !hideCollapseItemTooltip ||
-                            (id !== COLLAPSE_ITEM_ID && type !== 'action'),
-                    )
-                    .map((item, idx) => {
-                        switch (item.type) {
-                            case 'divider':
-                                return (
-                                    <div className={b('item', {divider: true})} key={idx}>
-                                        {item.title}
-                                    </div>
-                                );
-                            default:
-                                return (
-                                    <div
-                                        className={b('item', {
-                                            active: item === activeItem,
-                                        })}
-                                        key={idx}
-                                    >
-                                        {item.title}
-                                    </div>
-                                );
-                        }
-                    })}
+                <div className={b('items-container')}>
+                    {items
+                        .filter(
+                            ({type = 'regular', id}) =>
+                                !hideCollapseItemTooltip ||
+                                (id !== COLLAPSE_ITEM_ID && type !== 'action'),
+                        )
+                        .map((item, idx) => {
+                            switch (item.type) {
+                                case 'divider':
+                                    return (
+                                        <div className={b('item', {divider: true})} key={idx}>
+                                            {item.title}
+                                        </div>
+                                    );
+                                default:
+                                    return (
+                                        <div
+                                            className={b('item', {
+                                                active: item === activeItem,
+                                            })}
+                                            key={idx}
+                                        >
+                                            {item.title}
+                                        </div>
+                                    );
+                            }
+                        })}
+                </div>
             </div>
         </Popup>
     );
