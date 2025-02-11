1c5a674b3e82df6d68004f652aacb92d008ea47f
Angelina Gadzhieva <lunory@yandex-team.ru>
2025-02-11 14:29:11 +0300

    fix: fix toaster after update uikit@7 (#379)

--- playwright/playwright/index.tsx
+++ playwright/playwright/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-import {MobileProvider, ThemeProvider, ToasterComponent, ToasterProvider} from '@gravity-ui/uikit';
+import {
+    MobileProvider,
+    ThemeProvider,
+    Toaster,
+    ToasterComponent,
+    ToasterProvider,
+} from '@gravity-ui/uikit';
 import {beforeMount} from '@playwright/experimental-ct-react/hooks';
 
 import './index.scss';
+
+const toaster = new Toaster();
 
 beforeMount(async ({App}) => {
     return (
         <ThemeProvider>
             <MobileProvider>
-                <ToasterProvider>
+                <ToasterProvider toaster={toaster}>
                     <App />
                     <ToasterComponent />
                 </ToasterProvider>
