a5ad94b230e1b49f2f50e0fc771f2e78fe4af36e
Vlad Furman <blowlyge@gmail.com>
2025-05-14 17:52:48 +0300

    fix: added DOMProps & QAProps to ActionBar components (#430)
    * fix: added DOMProps & QAProps to ActionBar components
    
    * fix: added new props to README.md

--- src/components/ActionBar/Group/ActionBarGroup.tsx
+++ src/components/ActionBar/Group/ActionBarGroup.tsx
@@ -1,22 +1,30 @@
 import React, {PropsWithChildren} from 'react';
 
+import {DOMProps, QAProps} from '@gravity-ui/uikit';
+
 import {block} from '../../utils/cn';
 import {PropsWithPull} from '../types';
 
 import './ActionBarGroup.scss';
 
-export type Props = PropsWithChildren<
-    PropsWithPull<{
-        className?: string;
-        stretchContainer?: boolean;
-    }>
->;
+export type Props = DOMProps &
+    QAProps &
+    PropsWithChildren<
+        PropsWithPull<{
+            stretchContainer?: boolean;
+        }>
+    >;
 
 const b = block('action-bar-group');
 
-export const ActionBarGroup = ({children, className, pull, stretchContainer}: Props) => {
+export const ActionBarGroup = ({children, className, style, qa, pull, stretchContainer}: Props) => {
     return (
-        <ul className={b({pull, 'stretch-container': stretchContainer}, className)} role="group">
+        <ul
+            className={b({pull, 'stretch-container': stretchContainer}, className)}
+            role="group"
+            style={style}
+            data-qa={qa}
+        >
             {children}
         </ul>
     );
--- src/components/ActionBar/Section/ActionBarSection.tsx
+++ src/components/ActionBar/Section/ActionBarSection.tsx
@@ -1,16 +1,18 @@
 import React, {PropsWithChildren} from 'react';
 
+import {DOMProps, QAProps} from '@gravity-ui/uikit';
+
 import {block} from '../../utils/cn';
 
 import './ActionBarSection.scss';
 
-export type Props = PropsWithChildren<{type?: 'primary' | 'secondary'}>;
+export type Props = DOMProps & QAProps & PropsWithChildren<{type?: 'primary' | 'secondary'}>;
 
 const b = block('action-bar-section');
 
-export const ActionBarSection = ({children, type = 'primary'}: Props) => {
+export const ActionBarSection = ({children, className, style, qa, type = 'primary'}: Props) => {
     return (
-        <div className={b({type})} role="menu">
+        <div className={b({type}, className)} style={style} role="menu" data-qa={qa}>
             {children}
         </div>
     );
