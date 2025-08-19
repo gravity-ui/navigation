1636ae3d1b5977161ed093d328481a29abd03a54
Roman Barlos <darkgenius@nextmail.ru>
2025-08-19 20:59:57 +0300

    fix(MultipleTooltip): fix multiple tooltips css

--- src/components/CompositeBar/MultipleTooltip/MultipleTooltip.tsx
+++ src/components/CompositeBar/MultipleTooltip/MultipleTooltip.tsx
@@ -30,15 +30,13 @@ export const MultipleTooltip: React.FC<MultipleTooltipProps> = ({
     return (
         <Popup
             open={open}
-            floatingClassName={b('popup')}
+            className={b('popup')}
             anchorRef={anchorRef}
             strategy="fixed"
             placement={placement}
             offset={POPUP_OFFSET}
         >
-            <div className={b()}
-                data-theme-root={typeof window !== 'undefined' ? document.body.className : undefined}
-            >
+            <div className={b()}>
                 <div className={b('items-container')}>
                     {items
                         .filter(
