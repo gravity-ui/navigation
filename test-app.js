32df540e1d4a85dc736d54fcc26c8b4db250b05b
Roman Barlos <darkgenius@nextmail.ru>
2025-08-22 00:34:12 +0300

    fix: stabilize CSS Modules mapping and Storybook SCSS config
    - Map multi-class tokens in cn utility
    - Fix MultipleTooltip CSS and transparency
    - Use object classNames in Drawer transitions
    - Adjust rollup extractComponentCss plugin and tests
    - Update test Next.js app and checklist

--- test-app.js
+++
@@ -1,9 +0,0 @@
-// Тестовое приложение для проверки работы CSS-модулей
-import {Logo} from './build/esm/components/Logo/Logo.js';
-
-console.log('Logo component imported successfully');
-console.log('CSS classes should be scoped:', Logo);
-
-// Проверим, что CSS-модули работают корректно
-import styles from './build/esm/components/Logo/Logo.module.scss.js';
-console.log('CSS modules:', styles);
