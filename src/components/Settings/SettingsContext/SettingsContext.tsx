6af7b678d7f553a75ff8357a965179580efe99fe
Alexey <sudilovskiy@yandex-team.ru>
2025-10-20 12:26:17 +0100

    refactor(Settings): split Settings file to multiple modules (#476)

---
+++ src/components/Settings/SettingsContext/SettingsContext.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import type {SettingsContextType} from './types';
+
+export const SettingsContext = React.createContext<SettingsContextType>({});
--- src/components/Settings/index.ts
+++ src/components/Settings/index.ts
@@ -1,2 +1,12 @@
 export * from './Selection';
 export * from './Settings';
+export type {SettingsContextType} from './SettingsContext/types';
+export {useSettingsContext} from './SettingsContext/useSettingsContext';
+
+export type {
+    SettingsProps,
+    SettingsGroupProps,
+    SettingsPageProps,
+    SettingsSectionProps,
+    SettingsItemProps,
+} from './types';
