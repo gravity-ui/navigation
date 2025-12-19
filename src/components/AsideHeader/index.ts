cad53f3904e844a5dd42a0a395f9649f32508ca6
BlackPoretsky <20vinipuh02@gmail.com>
2025-12-19 13:02:20 +0300

    fix: add AsideHeaderItem type export to AsideHeader component

--- src/components/AsideHeader/index.ts
+++ src/components/AsideHeader/index.ts
@@ -1,5 +1,5 @@
 export {AsideHeader} from './AsideHeader';
-export type {AsideHeaderProps, PanelItemProps} from './types';
+export type {AsideHeaderProps, AsideHeaderItem, PanelItemProps} from './types';
 export {AsideHeaderContextProvider, useAsideHeaderContext} from './AsideHeaderContext';
 export {FooterItem, type FooterItemProps} from './components/FooterItem/FooterItem';
 export {PageLayout, type PageLayoutProps} from './components/PageLayout/PageLayout';
