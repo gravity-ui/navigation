0355829f60a30ec304ebe08cee8be67dc18af460
olaslo <skuf962@yandex.com>
2025-12-19 13:43:35 +0100

    fix: linter errors

--- src/components/AsideHeader/components/AllPagesPanel/index.ts
+++ src/components/AsideHeader/components/AllPagesPanel/index.ts
@@ -1,3 +1,2 @@
 export {AllPagesPanel} from './AllPagesPanel';
 export {getAllPagesMenuItem} from './constants';
-export {useVisibleMenuItems} from './useVisibleMenuItems';
--- src/components/AsideHeader/components/CompositeBar/CompositeBar.tsx
+++ src/components/AsideHeader/components/CompositeBar/CompositeBar.tsx
@@ -17,7 +17,7 @@ import styles from './CompositeBar.module.scss';
 
 const b = createBlock('composite-bar', styles);
 
-export type CompositeBarProps = {
+type CompositeBarProps = {
     type: 'menu' | 'subheader';
 
     items?: MenuItemsWithGroups[];
