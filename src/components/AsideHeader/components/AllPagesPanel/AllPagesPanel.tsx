7090fc744d7d3ff7024e2a181b25195f6f7d1e26
olaslo <skuf962@yandex.com>
2026-02-27 11:57:33 +0100

    feat: added support collapsedByDefault props

--- src/components/AsideHeader/components/AllPagesPanel/AllPagesPanel.tsx
+++ src/components/AsideHeader/components/AllPagesPanel/AllPagesPanel.tsx
@@ -53,7 +53,7 @@ export const AllPagesPanel: React.FC<AllPagesPanelProps> = (props) => {
 
         items.forEach((item) => {
             if ('groupId' in item && item.groupId && 'isCollapsed' in item) {
-                initial[item.groupId] = false;
+                initial[item.groupId] = item.collapsedByDefault ?? false;
             }
         });
         return initial;
